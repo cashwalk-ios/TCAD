c3f8a9c9cd26dfdfc9062c5b94d4115da7438f07
차윤오 <chayo65@cashwalk.io>
2023-08-08 15:15:17 +0900

    Add 프로젝트 생성, tca spm 추가

---
+++ TCAD/TCADApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCADApp.swift
+//  TCAD
+//
+//  Created by 차윤오 on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct TCADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
